fbebb761b1deab459cb45ae0eb9110f91ea36acc
Cedjuani <ggwepq@gmail.com>
2026-01-08 08:04:37 +0800

    init: home page done

---
+++ src/components/CursorGradient.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from "react";
+
+const CursorGradient = () => {
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    const cursor = cursorRef.current;
+
+    let mouseX = 0;
+    let mouseY = 0;
+    let cursorX = 0;
+    let cursorY = 0;
+
+    const speed = 0.15;
+
+    const animate = () => {
+      let distX = mouseX - cursorX;
+      let distY = mouseY - cursorY;
+
+      cursorX = cursorX + distX * speed;
+      cursorY = cursorY + distY * speed;
+
+      if (cursor) {
+        cursor.style.left = cursorX + "px";
+        cursor.style.top = cursorY + "px";
+      }
+
+      requestAnimationFrame(animate);
+    };
+
+    const handleMouseMove = (event) => {
+      mouseX = event.clientX;
+      mouseY = event.clientY;
+    };
+
+    animate();
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return <div ref={cursorRef} className="bg-gradient"></div>;
+};
+
+export default CursorGradient;
